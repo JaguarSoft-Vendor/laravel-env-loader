1352ee2400ec3b6dfd21db472e037af86c8709c2
LaurenceHR <laurencehr@gmail.com>
2022-04-20 18:15:44 -0500

    provider

---
+++ src/Provider/AppBindProvider.php
@@ -0,0 +1,33 @@
+<?php 
+namespace JaguarSoft\LaravelEnvLoader\Provider;
+
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Container\Container;
+
+use JaguarSoft\LaravelEnvLoader\Business\VarEnvBusiness;
+use JaguarSoft\LaravelEnvLoader\Contract\VarEnvService;
+use JaguarSoft\LaravelEnvLoader\Service\VarEnvDatabaseService;
+use JaguarSoft\LaravelEnvLoader\Service\VarEnvYamlService;
+use JaguarSoft\LaravelEnvLoader\Business\JaguarVarBusiness;
+
+class AppBindProvider extends ServiceProvider {
+    
+    public function register()
+    {
+        $this->app->bind(
+            'JaguarSoft\LaravelEnvLoader\Contract\VarEnvService',
+            [
+            'yaml' => 'JaguarSoft\LaravelEnvLoader\Service\VarEnvYamlService',
+            'database' => 'JaguarSoft\LaravelEnvLoader\Service\VarEnvDatabaseService'
+            ][env('VARENV_SERVICE','yaml')]
+        );
+
+        $this->app->singleton('JaguarVarBusiness', function(Container $container){        
+            return $container->make(JaguarVarBusiness::class);
+        });
+    }
+
+    public function boot() 
+    {
+    }
+}
