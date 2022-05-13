f05d23720d66261d5daff6894850aab610da53d9
LaurenceHR <laurencehr@gmail.com>
2022-05-12 23:06:01 -0500

    ifElse functions

--- src/Business/JaguarVarBusiness.php
+++ src/Business/JaguarVarBusiness.php
@@ -11,14 +11,14 @@ class JaguarVarBusiness {
 		$this->business = new VarEnvBusiness($service);
 	}
 
-	function env($codigo, $default = null, $valor = null) {		
-		$keys = explode(".", $codigo);
+	function env($key, $default = null, $val = null) {		
+		$keys = explode(".", $key);
 		if(count($keys) > 1) {
 			$pkey = array_shift($keys);			
 			if(!$this->business->hasOrEnv($pkey)) return $default;			
 			return array_get($this->business->getOrEnv($pkey), implode('.', $keys), $default);
 		} else {
-			return $this->business->getOrEnv($codigo, $default);
+			return $this->business->getOrEnv($key, $default);
 		}
 	}
 
@@ -29,6 +29,29 @@ class JaguarVarBusiness {
 			//$envs[$VarEnv->codigo] = $VarEnv;
 		}
 		return $envs;
+	}
+
+	protected function ifElse($key, $comp, callable $compare = null, callable $if = null, callable $else = null) {
+		$resp = call_user_func($compare, $key, $comp);
+		if ($resp === true) {
+			if(!is_null($if)) call_user_func($if, $key, $comp, $this->env($key));				
+		} else {
+			if(!is_null($else)) call_user_func($else, $key, $comp, $this->env($key));
+		}
+		return $resp;
+	}
+
+	public function ifEquals($key, $comp, callable $callback = null, callable $else = null) {
+		return $this->ifElse($key, $comp, function() use ($key, $comp) {
+			return $this->env($key) === $comp;
+		}, $callback, $else);
+	}
+
+	public function ifInArray($key, $arr, callable $callback = null, callable $else = null) {
+		if(!is_array($arr)) throw new \Exception('Array is Required');		
+		return $this->ifElse($key, $arr, function() use ($key, $arr) {
+			return in_array($this->env($key), $arr);
+		}, $callback, $else);
 	}
    
 }	
